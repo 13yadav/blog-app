f3aaa711b2161a1c3555703a21629f4530f95906
Ranjit Yadav <yadavranjit521@gmail.com>
2024-02-12 22:54:28 +0530

    create appwrite AuthService.js

---
+++ src/appwrite/auth.js
@@ -0,0 +1,63 @@
+import config from "../config/config.js";
+
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+
+      if (userAccount) {
+        this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async logout() {
+    try {
+      return await this.account.deleteSessions();
+    } catch (error) {
+      return error;
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
