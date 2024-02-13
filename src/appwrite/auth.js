64fd9fcf3854b3b61d8e5dfede3bf79071a925ec
Ranjit Yadav <yadavranjit521@gmail.com>
2024-02-13 22:44:44 +0530

    fix: await issue

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -24,7 +24,7 @@ export class AuthService {
       );
 
       if (userAccount) {
-        this.login({ email, password });
+        return await this.login({ email, password });
       } else {
         return userAccount;
       }
