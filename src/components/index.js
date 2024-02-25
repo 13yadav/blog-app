282bc7db0ad70cc35d2bf44f3ce27ceccaad13a8
Ranjit Yadav <yadavranjit521@gmail.com>
2024-02-25 12:46:46 +0530

    added component imports top global index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,21 +1,31 @@
-import Container from "./container/Container.jsx"
-import Header from "./Header/Header.jsx"
-import Footer from "./Footer/Footer.jsx"
-import Logo from "./Logo.jsx"
-import LogoutButton from "./Header/LogoutButton";
+import Container from "./container/Container.jsx";
+import Header from "./Header/Header.jsx";
+import LogoutButton from "./Header/LogoutButton.jsx";
+import Footer from "./Footer/Footer.jsx";
+import Logo from "./Logo.jsx";
 import Select from "./Select.jsx";
 import Button from "./Button.jsx";
 import Input from "./Input.jsx";
 import RTE from "./RTE.jsx";
+import AuthLayout from "./AuthLayout.jsx";
+import Login from "./Login.jsx";
+import Signup from "./Signup.jsx";
+import PostCard from "./PostCard.jsx";
+import PostForm from "./post-form/PostForm.jsx";
 
 export {
-    Header,
-    Footer,
-    Logo,
-    LogoutButton,
-    Container,
-    Select,
-    Button,
-    Input,
-    RTE,
-}
+  Header,
+  Footer,
+  Logo,
+  LogoutButton,
+  Container,
+  Select,
+  Button,
+  Input,
+  RTE,
+  AuthLayout,
+  Login,
+  Signup,
+  PostCard,
+  PostForm,
+};
